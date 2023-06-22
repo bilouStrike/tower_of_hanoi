34bcad93fbf5fba9f3dc9a60930d3602391df0c8
Bilal <bilal89dif@gmail.com>
2023-06-22 09:38:14 +0200

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Tower_of_Hanoi/',
+  base: '/tower_of_hanoi/',
   plugins: [react()],
 })
